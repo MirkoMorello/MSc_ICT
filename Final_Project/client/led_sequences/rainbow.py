6f2cfcb7d0ca9de66d9ecd74a78a65df6b4daba5
unknown <deuterio211@gmail.com>
2025-01-27 02:27:08 +0100

    Small fixes to the objects hierarchy

--- Final_Project/client/led_sequences/rainbow.py
+++ Final_Project/client/led_sequences/rainbow.py
@@ -1,18 +1,29 @@
-from led_sequences.base_sequence_rotation import BaseSequenceRotation
+from led_sequences.base_sequence import BaseSequence
 import time 
 
-class Rainbow(BaseSequenceRotation):
+class Rainbow(BaseSequence):
     def __init__(self, brightness = 0.5):
         super().__init__()
         self.brightness = brightness
 
+    def __wheel(self, pos):
+        """Generate rainbow colors across 0-255 positions."""
+        if pos < 85:
+            return (pos * 3, 255 - pos * 3, 0)
+        elif pos < 170:
+            pos -= 85
+            return (255 - pos * 3, 0, pos * 3)
+        else:
+            pos -= 170
+            return (0, pos * 3, 255 - pos * 3)
+        
     def sequence(self, semaphore):
         """Draw rainbow that uniformly distributes itself across all pixels."""
         while semaphore.is_keep_going():
             for j in range(256):
                 led_data = []
                 for i in range(self.get_led_count()):
-                    color = self._wheel((int(i * 256 / self.get_led_count()) + j) & 255)
+                    color = self.__wheel((int(i * 256 / self.get_led_count()) + j) & 255)
                     r, g, b = [int(c * self.brightness) for c in color]
                     led_data.append([0xE0 | int(self.brightness*31), b, g, r]) # Brightness + BGR
                 self._write(led_data)
--- Final_Project/client/led_sequences/rotating.py
+++ Final_Project/client/led_sequences/rotating.py
@@ -1,18 +1,18 @@
-from led_sequences.base_sequence_rotation import BaseSequenceRotation
+from led_sequences.base_sequence import BaseSequence
 import time
 
-class BlinkRotation(BaseSequenceRotation):
-    def __init__(self, brightness = 0.5):
+class Rotating(BaseSequence):
+    def __init__(self, color, brightness = 0.5):
         super().__init__()
         self.brightness = brightness
+        self.color = color
          
     def sequence(self, semaphore):
         while(semaphore.is_keep_going()):
             led_state = []
             for i in range(self.get_led_count()):
                 # Get the rainbow color for this position
-                color = self._wheel(int(i * 256 / self.get_led_count()) & 255)
-                r, g, b = color
+                r, g, b = self.color
 
                 led_state = ([[0xE0, 0x00, 0x00, 0x00]] * i) +  [[0xE0 | int(self.brightness * 31), b, g, r]] + ([[0xE0, 0x00, 0x00, 0x00]] * (self.get_led_count() - i - 1))
 
