654506e123917b3f26f13251427e73672fb4eecc
Mirko Morello <mirkomorello97@gmail.com>
2025-02-15 18:44:31 +0100

    test_network.py

---
+++ Final_Project/client/utils.py
@@ -0,0 +1,8 @@
+from EncDecClassificationModel import EncDecClassificationModel
+import torch
+
+def get_model(path): 
+    model = EncDecClassificationModel(num_classes=35, sample_rate=16000)
+    state_dict = torch.load(path, map_location=torch.device('cpu'))
+    model.load_state_dict(state_dict)
+    return model
