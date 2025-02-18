b483f66a02291b83bfac13089fde282fad52bbcf
Mirko Morello <mirkomorello97@gmail.com>
2025-02-18 18:44:23 +0100

    removed unused objects

--- Final_Project/client/client.py
+++ Final_Project/client/client.py
@@ -141,9 +141,7 @@ class Client:
         self.labels = labels
         self.overlap_buffer = torch.tensor([], dtype=torch.float32)
         self.overlap_samples = int(self.audio_params["rate"] * 0.25)
-        self.previous_chunk = None  # store the previous chunk
-        self.led_wake_word = Fixed(Colors.BLUE) 
-        self.led_waiting_wake_word = Rainbow()
+        self.previous_chunk = None  # store the previous chunk 
 
     def _connect_to_server(self):
         try:
